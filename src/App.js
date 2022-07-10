df7138760a613faf6731644bc9fc45210c7cca6f
Abhay <leader.abhay002@gmail.com>
2022-07-10 14:13:41 +0530

    algolia search engine applied

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import { useEffect, useState } from "react";
-import Product from "./components/Product";
+import "instantsearch.css/themes/satellite.css";
+import algoliasearch from "algoliasearch/lite";
+import { Hits, InstantSearch, SearchBox } from "react-instantsearch-hooks-web";
+import product from "./components/Product";
+
 function App() {
+  const searchClient = algoliasearch(
+    "S7Z8AD7A1J", // API Keys -- will be save in .env file later
+    "4ba70fde430a86db64ac81bc151e12d2"
+  );
+  // const index = searchClient.initIndex('react-test-2');
+
   const [Products, setProducts] = useState([]);
   useEffect(() => {
     async function getRecords() {
@@ -16,18 +26,26 @@ function App() {
     return;
   }, []);
 
+  // index.savebjects(Products);
+
   return (
     <div className="max-w-8xl mx-auto">
-      <div>{/* Search Bar */}</div>
-      <h1 className=" py-2 text-5xl font-bold text-center">Products List</h1>
-      <hr/>
-      <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 space-x-4 space-y-4 m-4">
-        {Products.map((product) => {
-          return <Product props={product} />;
-        })}
+      <h1 className=" py-2 text-5xl font-bold text-center">Products List</h1>{" "}
+      <div className="items-center text-center">
+        <InstantSearch searchClient={searchClient} indexName="react-test">
+          <SearchBox className="p-2 " />
+          <Hits className="text-center" hitComponent={product} />
+        </InstantSearch>
       </div>
+      <hr />
     </div>
   );
 }
 
 export default App;
+
+//  {/* <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 space-x-4 space-y-4 m-4">
+//         {Products.map((product) => {
+//           return <Product hit={product} />;
+//         })}
+//       </div> */}
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,28 +1,33 @@
 import React from "react";
+import { Highlight } from "react-instantsearch-hooks-web";
 
-function Product({ props }) {
-  // console.log(props);
+function product({ hit }) {
+  console.log(hit);
   return (
     <div className="items-center text-center p-2 rounded-lg border hover:border-gray-600 cursor-pointer">
       <div className="space-x-2 text-center py-4 ite">
         <span className="bg-black text-white rounded-full px-2">
-          {props.Type}
+          {hit.Type}
         </span>
-        <span className="bg-black text-white rounded-full px-2">
-          {props.SKU}
-        </span>
+        <span className="bg-black text-white rounded-full px-2">{hit.SKU}</span>
       </div>
+      <h1>
+        <Highlight attribute="Name" hit={hit} />
+      </h1>
       <img
         className="h-40 w-30 shadow-sm object-cover mx-auto"
-        src={props.Images ? props.Images : "https://media.istockphoto.com/photos/elegance-peach-vintage-dress-isolated-on-white-background-picture-id1217970962?k=20&m=1217970962&s=612x612&w=0&h=XJeWZiOrycXuEawk2SnjXqCFpjZ9mMUPNKJqnx82ziU="}
+        src={
+          hit.Images
+            ? hit.Images
+            : "https://media.istockphoto.com/photos/elegance-peach-vintage-dress-isolated-on-white-background-picture-id1217970962?k=20&m=1217970962&s=612x612&w=0&h=XJeWZiOrycXuEawk2SnjXqCFpjZ9mMUPNKJqnx82ziU="
+        }
         alt="product"
       />
       <div>
-        <h2 className="text-lg text-gray-500 hover:text-black">{props.Name}</h2>
-        {/* <p>{props.description}</p> */}
+        <h2 className="text-lg text-gray-500 hover:text-black">{hit.Name}</h2>
       </div>
     </div>
   );
 }
 
-export default Product;
+export default product;
